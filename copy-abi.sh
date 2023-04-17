47c13a5d05929530bc5c7bc706600d49e543bcdb
realbits-lab <dev.realbits@gmail.com>
2023-04-17 16:51:55 +0900

    Add smart contract abi files.

---
+++ copy-abi.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cp -f ../rent-market/artifacts/contracts/rentNFT.sol/rentNFT.json contracts/
+cp -f ../rent-market/artifacts/contracts/promptNFT.sol/promptNFT.json contracts/
+cp -f ../rent-market/artifacts/contracts/rentMarket.sol/rentMarket.json contracts/
