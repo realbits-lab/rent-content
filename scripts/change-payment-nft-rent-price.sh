9791d238a5ae10dcfe84171ca6c5e39cce1164b1
realbits-lab <dev.realbits@gmail.com>
2023-07-21 11:02:20 +0900

    Add change payment nft rent price shell script for testing.

---
+++ scripts/change-payment-nft-rent-price.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Export .env variables.
+while read line; do
+	firstCharacter=${line:0:1}
+	#* Check the first character with "N" for NEXT_PUBLIC_ variable name.
+	[[ "$firstCharacter" =~ [N] ]] && export $line
+done <.env
+
+# echo ${NEXT_PUBLIC_SETTLE_AUTH_KEY}
+
+curl -d "{\"auth_key\":\"${NEXT_PUBLIC_SETTLE_AUTH_KEY}\", \"usd_amount\":\"0.1\"}" \
+	-H "Content-Type: application/json" \
+	-X POST http://localhost:3001/api/change-payment-nft-rent-price
